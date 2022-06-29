90c1b1cf929c8c3c0639115daca669f445d0fbdd
teddyrugged <jakpaetuk@gmail.com>
2022-06-30 00:43:15 +0100

    update

--- blogapi/blog_project/urls.py
+++ blogapi/blog_project/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('posts.urls')),
+    path('api/v1/', include('posts.urls')), #postlist
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', 
-        include('dj_rest_auth.registration.urls')),
+        include('dj_rest_auth.registration.urls')), #register
 ]
--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import PostList, PostDetail
 
 urlpatterns = [
+    # path('users/', UserList.as_view()), # new
+    # path('users/<int:pk>/', UserDetail.as_view()),
     path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
 ]
